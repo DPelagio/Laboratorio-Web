098186ee3bd876bb045b5bc8cf89dbecd1d18dfd
DPelagio <pelagio3@gmail.com>
2020-08-31 08:55:53 -0500

    Project template

---
+++ project/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import Home from "./Pages/Home";
+import User from './Pages/User';
+import Landing from "./Pages/Landing";
+
+import NavBar from "./Components/NavBar";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {Widget, addResponseMessage} from 'react-chat-widget';
+
+import 'react-chat-widget/lib/styles.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const handleNewUserMessage = (newMessage) =>{
+  console.log(newMessage);
+  addResponseMessage('Hola -> ' + newMessage);
+}
+
+export default function App() {
+  return (
+    <Router>
+      <NavBar />
+      <Switch>
+      <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/user">
+          <User />
+        </Route>
+      </Switch>
+      <Widget handleNewUserMessage={handleNewUserMessage} 
+              title="TEC"
+              subtitle="And my cool subtitle!"/>
+    </Router>
+  );
+}
