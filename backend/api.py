a29228f106dba768aef29c01eab510fbc35c83e5
romeova <romeova.7243@gmail.com>
2020-09-07 12:18:33 -0500

    IBM watson assistant connected with the api, Requests from React

--- backend/api.py
+++ backend/api.py
@@ -23,12 +23,124 @@ api = Api(app)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 assistant_api_key = os.getenv("assistant_api_key")
+assistant_url = os.getenv("assistant_url")
+assistant_version = os.getenv("assistant_version")
+assistant_id = os.getenv("assistant_id")
+
+request_data = {
+            "assistant_api_key": assistant_api_key,
+            "assistant_url": assistant_url,
+            "assistant_version": assistant_version,
+            "assistant_id": assistant_id
+        }
+
+
+def watson_create_session():
+
+
+    
+    iam_apikey = request_data.get("assistant_api_key")
+    assistant_url = request_data.get("assistant_url")
+    assistant_version = request_data.get('assistant_version')
+
+    assistant = watson_instance(iam_apikey, assistant_url, assistant_version)
+
+    try:
+        watson_session = assistant.create_session(
+            assistant_id=request_data.get("assistant_id")
+        ).get_result()
+        watson_session_id = watson_session["session_id"]
+    except KeyError:
+        _logger.error("The session wasn't created")
+        return jsonify({"error": "Error creating the session"}), status.HTTP_503_SERVICE_UNAVAILABLE
+
+    return watson_session_id
+
+
+def watson_response(watson_session_id,message):
+    
+    iam_apikey = request_data.get("assistant_api_key")
+    assistant_url = request_data.get("assistant_url")
+    assistant_version = request_data.get('assistant_version')
+
+    assistant = watson_instance(iam_apikey, assistant_url, assistant_version)
+    context = request_data.get('context') if 'context' in request_data else {}
+
+    try:
+        watson_response = assistant.message(
+            assistant_id=request_data.get('assistant_id'),
+            session_id=watson_session_id,
+            input={
+                'message_type': 'text',
+                'text': message,
+                'options': {
+                    'return_context': True
+                }
+            },
+            context=context
+        ).get_result()
+    except ValueError as ex:
+        _logger.error("Value error: %s", ex)
+        return jsonify({'error': "Value error"}), status.HTTP_500_INTERNAL_SERVER_ERROR
+    except ApiException:
+        try:
+            watson_session = assistant.create_session(
+                assistant_id=request_data.get("assistant_id")
+            ).get_result()
+            watson_session_id = watson_session["session_id"]
+
+            watson_response = assistant.message(
+                assistant_id=request_data.get('assistant_id'),
+                session_id=watson_session_id,
+                input={
+                    'message_type': 'text',
+                    'text': request_data.get('input_message'),
+                    'options': {
+                        'return_context': True
+                    }
+                },
+                context=context
+            ).get_result()
+        except KeyError:
+            _logger.error("The session wasn't created")
+            return jsonify({"error": "Error creating the session"}), status.HTTP_503_SERVICE_UNAVAILABLE
+
+    try:
+        del watson_response["context"]["global"]["session_id"]
+    except:
+        pass
+
+    response = {
+        "response": watson_response["output"]["generic"][0]["text"],
+        "session_id": watson_session_id
+    }
+
+    return response
+
+def watson_instance(iam_apikey: str, url: str, version: str = "2020-08-31") -> AssistantV2:
+    try:
+        authenticator = IAMAuthenticator(iam_apikey)
+        assistant = AssistantV2(
+            authenticator=authenticator,
+            version=version
+        )
+        assistant.set_service_url(url)
+    except ApiException as error:
+        _logger.error("%s - %s", error.code, error.message)
+        return jsonify({'error': str(error.message)}), error.code
+
+    return assistant
+
 
 
 class GET_MESSAGE(Resource):
     def post(self):
 
-        return jsonify( este_es_el_mensaje = request.json["message"])
+        watson_session_id = watson_create_session()
+
+        response = watson_response(watson_session_id,request.json["message"])
+
+        return jsonify( este_es_el_mensaje = response)
 
 
 api.add_resource(GET_MESSAGE, '/getMessage')  # Route_1
