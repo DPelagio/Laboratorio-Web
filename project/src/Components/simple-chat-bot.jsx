1f6017dd0de8b42b497c26f6638caa4a67a1779f
romeova <romeova.7243@gmail.com>
2020-09-15 21:22:02 -0500

    Html injection from watson, styled chat-bot

--- project/src/Components/simple-chat-bot.jsx
+++ project/src/Components/simple-chat-bot.jsx
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ChatBot from 'react-simple-chatbot';
 import axios from 'axios'
+import { ThemeProvider } from 'styled-components';
 import parse from 'html-react-parser';
+import { Carousel ,Card} from "react-bootstrap";
+import image from "../img/ipad.png";
+import JsxParser from 'react-jsx-parser'
+
+//Styles
+import "./chat-bot-bubble.scss"
+
 
 class Review extends Component {
   constructor(props) {
@@ -37,8 +45,47 @@ class Review extends Component {
 
   render() {
     const { message,response} = this.state;
+
     return (
-        parse(response)
+      parse(response)
+
+      /*
+<Carousel>
+<Carousel.Item>
+  <Card style={{ width: '100%' }}>
+  <Card.Img style={{ height: '200px' }} variant="top" src={image} />
+  <Card.Body>
+    <Card.Title>Ipad 32 gb</Card.Title>
+    <Card.Text>
+      &#36;299
+    </Card.Text>
+  </Card.Body>
+  </Card>
+</Carousel.Item>
+<Carousel.Item>
+ <Card style={{ width: '100%' }}>
+  <Card.Img style={{ height: '200px' }} variant="top" src={image} />
+  <Card.Body>
+   <Card.Title>Ipad 64 gb</Card.Title>
+   <Card.Text>
+    &#36;399
+   </Card.Text>
+  </Card.Body>
+ </Card>
+</Carousel.Item>
+<Carousel.Item>
+ <Card style={{ width: '100%' }}>
+  <Card.Img style={{ height: '200px' }} variant="top" src={image} />
+   <Card.Body>
+    <Card.Title>Ipad 128 gb</Card.Title>
+    <Card.Text>
+     &#36;499
+    </Card.Text>
+   </Card.Body>
+  </Card>
+</Carousel.Item>
+</Carousel>
+*/
     );
   }
 }
@@ -53,32 +100,45 @@ Review.defaultProps = {
 
 class SimpleForm extends Component {
   render() {
+
+    const theme = {
+      background: '#e6f8ff',
+      fontFamily: 'Helvetica Neue',
+      headerBgColor: '#17c1ff',
+      headerFontColor: '#fff',
+      headerFontSize: '15px',
+      botBubbleColor: '#17c1ff',
+      botFontColor: '#fff',
+      userBubbleColor: '#fff',
+      userFontColor: '#4a4a4a',
+    };
+
     return (
-      <ChatBot
-        steps={[
-          {
-            id: '1',
-            message: 'Hi, what can I help you?',
-            trigger: 'message',
-          },
-          {
-            id: 'message',
-            user: true,
-            trigger: '3',
-          },
-          {
-            id: '3',
-            component: <Review />,
-            waitAction: false,
-            trigger: '4',
-          },
-          {
-            id: '4',
-            message: 'Que mas puedo hacer por ti?',
-            trigger: 'message',
-          },
-        ]}
-      />
+      <ThemeProvider theme={theme}>
+
+        <ChatBot
+          steps={[
+            {
+              id: '1',
+              message: 'Hi, what can I help you?',
+              trigger: 'message',
+            },
+            {
+              id: 'message',
+              user: true,
+              trigger: '3',
+            },
+            {
+              id: '3',
+              component: <Review />,
+              waitAction: false,
+              trigger: 'message',
+            }
+          ]}
+        />
+
+      </ThemeProvider>
+      
     );
   }
 }
